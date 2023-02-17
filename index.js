d0c6c24ea6b786037b37bb6c17e0592d52d8444e
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-17 10:09:28 -0300

    Feat: Selecting all data entry fields

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name");
+
+fields.forEach(function(field, index){
+
+    console.log(field.id, field.name, field.value, index);
+
+});
