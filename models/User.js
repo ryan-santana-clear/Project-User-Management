4c1681ee1f66fe15cdcd2224bc410bb1bc95cb97
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-22 09:08:48 -0300

    Feat: Creating User Class and Constructor

---
+++ models/User.js
@@ -0,0 +1,15 @@
+class User{
+
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = email;
+        this.passsword = password;
+        this.photo = photo;
+        this.admin = admin;
+
+    }
+
+}
