69fc260a4c3739ddf8e63fbcf238dbd928acd171
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-27 10:53:01 -0300

    Feat: Transferring the getUsersStorage method to the models folder

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -197,25 +197,9 @@ class UserController {
 
     }
 
-    getUsersStorage(){
-
-        let users = [];
-
-        //if (sessionStorage.getItem("users")) {
-        if (localStorage.getItem("users")) {
-
-            //users = JSON.parse(sessionStorage.getItem("users"));
-            users = JSON.parse(localStorage.getItem("users"));
-
-        }
-
-        return users;
-
-    }
-
     selectAll(){
 
-        let users = this.getUsersStorage();
+        let users = User.getUsersStorage();
 
         users.forEach(dataUser=>{
 
@@ -268,6 +252,12 @@ class UserController {
         tr.querySelector(".btn-delete").addEventListener("click", e=> {
 
             if (confirm("Deseja realmente excluir ?")) {
+
+                let user = new User();
+
+                user.loadFromJSON(JSON.parse(tr.dataset.user));
+
+                user.remove();
 
                 tr.remove();
                 
