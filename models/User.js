8e8427018e304af05e3a40905f10daa2c441ce72
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-27 08:59:49 -0300

    Feat: Implementing the generation of a new id for each new user

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ class User{
 
     constructor(name, gender, birth, country, email, password, photo, admin){
 
+        this._id;
         this._name = name;
         this._gender = gender;
         this._birth = birth;
@@ -12,6 +13,10 @@ class User{
         this._admin = admin;
         this._register = new Date();
 
+    }
+
+    get id(){
+        return this._id;
     }
 
     get register(){
@@ -68,7 +73,62 @@ class User{
             
             }
 
+
         }
+
+    }
+
+    static getUsersStorage() {
+
+        let users = [];
+        //if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
+            //users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
+
+        }
+
+        return users;
+
+    }
+
+    getNewId(){
+
+        if (!window.id) window.id = 0; 
+
+        id++;
+
+        return id;
+
+    }
+
+    save(){
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+
+            users.map(u=>{
+
+                if (u._id === this.id) {
+
+                    u = this;
+
+                }
+
+                return u;
+
+            });
+
+        } else {
+
+            this._id = this.getNewId();
+
+            users.push(this);
+            
+        }
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));    
 
     }
 
