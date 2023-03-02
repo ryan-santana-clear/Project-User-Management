7bae92e5bd48e960b47b5f91c1e4448d54a015ef
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-02 08:44:42 -0300

    Feat: Implementing DELETE using Ajax

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -80,29 +80,7 @@ class User{
 
     static getUsersStorage() {
 
-        let users = [];
-        //if (sessionStorage.getItem("users")) {
-        if (localStorage.getItem("users")) {
-            //users = JSON.parse(sessionStorage.getItem("users"));
-            users = JSON.parse(localStorage.getItem("users"));
-
-        }
-
-        return users;
-
-    }
-
-    getNewID(){
-
-        let usersID = parseInt(localStorage.getItem("usersID"));
-
-        if (!usersID > 0) usersID = 0; 
-
-        usersID++;
-
-        localStorage.setItem("usersID", usersID);
-
-        return usersID;
+        return HttpRequest.get('/users')
 
     }
 
@@ -154,19 +132,7 @@ class User{
 
     remove(){
 
-        let users = User.getUsersStorage();
-
-        users.forEach((userData, index)=>{
-
-            if (this._id == userData._id) {
-
-                users.splice(index, 1);
-
-            }
-
-        });
-
-        localStorage.setItem("users", JSON.stringify(users));    
+        return HttpRequest.delete(`/users/${this.id}`);
 
     }
 
